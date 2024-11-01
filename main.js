a7a71240f0c43a20dcb7feeb7b0a425837a49628
Gust40019 <146958235+Gust40019@users.noreply.github.com>
2024-11-01 08:42:27 -0300

    .

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ function criarCartao(pergunta, resposta){
        let cartao = document.createElement(article)
         cartao.classList = 'cartao'
 
-        cartao.innerHTML=
+        cartao.innerHTML= `
         <div class="cartao--conteudo"
-        <div class="cartao--pergunta"><p>Time com mais títulos no brasileirão</p> </div>
-        <div class="cartao--resposta"><p> Palmeiras 12 títulos </p></div>
-       </div>
+         <div class="cartao--pergunta"><p>${pergunta}</p> </div>
+        <div class="cartao--resposta"><p>${resposta}  </p></div>
+       </div>`
 
         conteudo.
 conteudo.appChild(cartao)
